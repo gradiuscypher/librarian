3aecc5dc363a23051b7ed21e9d886c3ceff32ec5
gradiuscypher@gmail.com <jared.rathbun@discordapp.com>
2021-06-19 23:55:22 -0700

    code

--- cogs/echo.py
+++ cogs/echo.py
@@ -1,6 +1,10 @@
+import csv
 import discord
 import logging
+import os
+import time
 from discord.ext import commands
+from glob import glob
 
 # setup logging
 logger = logging.getLogger("librarian")
@@ -15,10 +19,42 @@ class Echo(commands.Cog):
         """
         Echoes the message
         """
-        await ctx.send(input)
-        logger.error("This is a test")
-        logger.info(input)
+        current_files = glob('current*.csv')
 
+        # the current_<TIME>.csv file exists
+        if len(current_files) > 0:
+            target_name = current_files[0]
+            start_time = int(target_name.split('_')[1].split('.')[0])
+
+            # verify whether the current file should be uploaded and rotated
+            current_time = int(time.time())
+            if current_time - start_time >= 10:
+                start_new_csv(old_file=target_name)
+
+            # if it isn't old enough, add on a new row entry
+            else:
+                with open(target_name, 'a') as csv_file:
+                    csv_writer = csv.writer(csv_file, delimiter=',')
+                    csv_writer.writerow(["4321", "USERNAME", "USER DISCRIM", "TEST GAME", str(current_time), "TEST END"])
+
+        # the current file does not exist, create it.
+        else:
+            start_new_csv()
 
 def setup(bot):
     bot.add_cog(Echo(bot))
+
+def start_new_csv(old_file=None):
+    current_time = int(time.time())
+    # TODO: if an old_file name is provided, upload the old file and report to the user
+    # TODO: this file removal is broken
+    if old_file:
+        print("OLD FILE IS OLD")
+        full_path = os.path.join(os.getcwd(), old_file)
+        print(full_path)
+        os.remove(os.path.join(os.getcwd(), old_file))
+
+    with open(f"current_{current_time}.csv", 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        csv_writer.writerow(["User ID", "User Name", "User Discriminator", "Game Name", "Game Start Time", "Game End Time"])
+        csv_writer.writerow(["1234", "USERNAME", "USER DISCRIM", "TEST GAME", str(current_time), "TEST END"])
--- cogs/member_update.py
+++ cogs/member_update.py
@@ -3,13 +3,16 @@ ref: https://discordpy.readthedocs.io/en/latest/api.html#discord.ActivityType
 ref: https://discordpy.readthedocs.io/en/latest/api.html#discord.Member.activity
 """
 
+import csv
 import discord
 import logging
+import os
+import time
 from discord.ext import commands
+from glob import glob
 
 # setup logging
 logger = logging.getLogger("librarian")
-
 
 class MemberUpdate(commands.Cog):
     def __init__(self, bot) -> None:
@@ -17,11 +20,51 @@ class MemberUpdate(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        if after.activity:
-            if after.activity.type == discord.ActivityType.listening:
-                print(after.activity.album)
+        # TODO: make this work
+        if before is not None:
+            print(before.name, before.id, before.discriminator)
+            if before.activity is not None:
+                print(before.activity.name, before.activity.start)
+        if after is not None:
+            print(after.name, after.id, after.discriminator)
+            if after.activity is not None:
+                print(after.activity.name, after.activity.start)
+
+        if after is None and before:
+            current_files = glob('current*.csv')
+
+            # the current_<TIME>.csv file exists
+            if len(current_files) > 0:
+                target_name = current_files[0]
+                start_time = int(target_name.split('_')[1].split('.')[0])
+
+                # verify whether the current file should be uploaded and rotated
+                current_time = int(time.time())
+                if current_time - start_time >= 10:
+                    start_new_csv(old_file=target_name, before_obj=before)
+
+                # if it isn't old enough, add on a new row entry
+                else:
+                    with open(target_name, 'a') as csv_file:
+                        csv_writer = csv.writer(csv_file, delimiter=',')
+                        csv_writer.writerow([before.id, before.name, before.discriminator, before.activity.game, before.start, time.time()])
+
+            # the current file does not exist, create it.
             else:
-                print(after.activity.type)
+                start_new_csv(before_obj=before)
 
 def setup(bot):
     bot.add_cog(MemberUpdate(bot))
+
+
+def start_new_csv(old_file=None, before_obj=None):
+    current_time = int(time.time())
+    # TODO: if an old_file name is provided, upload the old file and report to the user
+
+    if old_file:
+        os.remove(os.path.join(os.getcwd(), old_file))
+
+    with open(f"current_{current_time}.csv", 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        csv_writer.writerow(["User ID", "User Name", "User Discriminator", "Game Name", "Game Start Time", "Game End Time"])
+        csv_writer.writerow([before_obj.id, before_obj.name, before_obj.discriminator, before_obj.activity.game, before_obj.start, time.time()])
